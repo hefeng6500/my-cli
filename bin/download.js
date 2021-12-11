a03acfa6f78808368fe19a912762c6b1c4484540
兰亭古墨 <2443992009@qq.com>
2021-12-11 21:29:24 +0800

    fix

--- bin/download.js
+++
@@ -1,7 +0,0 @@
-let download = require("download-git-repo");
-
-download("direct:https://github.com/hefeng6500/interview-blog#main", "test", {
-  clone: true,
-}, function (err) {
-  console.log(err)
-});
